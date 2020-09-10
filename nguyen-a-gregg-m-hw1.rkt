e6690287af6018bf66265bc2116eb4911ed2f896
Miles <milesgregg3@gmail.com>
2020-09-09 20:52:14 -0400

    Update nguyen-a-gregg-m-hw1.rkt

--- nguyen-a-gregg-m-hw1.rkt
+++ nguyen-a-gregg-m-hw1.rkt
@@ -3,11 +3,11 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname nguyen-a-gregg-m-hw1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;;Aaron Nguyen (anguyen3) , Miles Gregg (mgregg)
 ;; 1.
-(define-struct restaurant (name food tables seats vegetarian))
+(define-struct restaurant (name food tables seats vegetarian?))
 
-(define R1 (make-restaurant "Blank" "French" 10 40 true))
-(define R2 (make-restaurant "Blank1" "Pizza" 5 20 false))
-(define R3 (make-restaurant "Blank2" "Steakhouse" 50 250 true))
+(define R1 (make-restaurant "Le Pain" "French" 10 40 true))
+(define R2 (make-restaurant "Anthony" "Pizza" 5 20 false))
+(define R3 (make-restaurant "Longhorn" "Steakhouse" 50 250 true))
 
 ;; 2.
 ;;constructor
@@ -18,7 +18,7 @@
 ;;restaurant-food: restaurant -> String
 ;;restaurant-tables: restaurant -> Number
 ;;restaurant-seats: restaurant -> Number
-;;restaurant-vegetarian: restaurant -> Boolean
+;;restaurant-vegetarian?: restaurant -> Boolean
 
 ;;predicate:
 ;;restaurant?: Anytype -> Boolean
@@ -29,7 +29,7 @@
 ;;consumes the restaurant and produces the type of restaurant
 (define (restaurant-type restaurant)
   (cond [(and (>= (restaurant-tables restaurant) 30) (>= (restaurant-seats restaurant) 250)) "event venue"]
-        [(boolean=? (restaurant-vegetarian restaurant) true) "vegetarian-friendly"]
+        [(boolean=? (restaurant-vegetarian? restaurant) true) "vegetarian-friendly"]
         [else (restaurant-food restaurant)]))
 
 (check-expect (restaurant-type R1) "vegetarian-friendly")
@@ -37,12 +37,12 @@
 (check-expect (restaurant-type R3) "event venue")
 
 ;; 4.
-(define-struct date (month day year ))
+(define-struct date (month day year))
 (define-struct reservation (restaurant-name person phone date party))
 
-(define Res (make-reservation "Blank" "Bob" "111-212-3333" (make-date 9 6 2020) 12))
-(define Res1 (make-reservation "Blank1" "Joe" "123-212-3333" (make-date 9 4 2020) 10))
-(define Res2 (make-reservation "Blank2" "Lily" "511-212-3233" (make-date 9 9 2020) 4))
+(define Res (make-reservation "Le Pain" "Bob" "111-212-3333" (make-date 9 6 2020) 12))
+(define Res1 (make-reservation "Anthony" "Joe" "123-212-3333" (make-date 9 4 2020) 10))
+(define Res2 (make-reservation "Longhorn" "Lily" "511-212-3233" (make-date 9 9 2020) 4))
 
 ;; 5.
 ;;add-to-party: reservation Number -> reservation
@@ -55,9 +55,9 @@
                     (reservation-date reservation)
                     (+ (reservation-party reservation) people)))
 
-(check-expect (add-to-party Res 12) (make-reservation "Blank" "Bob" "111-212-3333" (make-date 9 6 2020) 24))
-(check-expect (add-to-party Res1 -6) (make-reservation "Blank1" "Joe" "123-212-3333" (make-date 9 4 2020) 4))
-(check-expect (add-to-party Res2 0) (make-reservation "Blank2" "Lily" "511-212-3233" (make-date 9 9 2020) 4))
+(check-expect (add-to-party Res 12) (make-reservation "Le Pain" "Bob" "111-212-3333" (make-date 9 6 2020) 24))
+(check-expect (add-to-party Res1 -6) (make-reservation "Anthony" "Joe" "123-212-3333" (make-date 9 4 2020) 4))
+(check-expect (add-to-party Res2 0) (make-reservation "Longhorn" "Lily" "511-212-3233" (make-date 9 9 2020) 4))
 
 ;; 6.
 
@@ -81,6 +81,7 @@
 (check-expect (precedes? (make-date 8 2 2020) (make-date 9 5 2020)) true)
 (check-expect (precedes? (make-date 9 2 2001) (make-date 9 5 2004)) true)
 (check-expect (precedes? (make-date 9 2 2010) (make-date 9 5 2008)) false)
+
 ;; 7.
 
 ;;reservation-OK?: reservation number -> boolean
