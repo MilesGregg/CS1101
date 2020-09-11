722279448eb0aa9d09696d46a3a9659822db1129
Miles <milesgregg3@gmail.com>
2020-09-10 22:04:39 -0400

    Update nguyen-a-gregg-m-hw1.rkt

--- nguyen-a-gregg-m-hw1.rkt
+++ nguyen-a-gregg-m-hw1.rkt
@@ -2,14 +2,26 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname nguyen-a-gregg-m-hw1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;;Aaron Nguyen (anguyen3) , Miles Gregg (mgregg)
+
+;;----------------------------------------------------------------------------------------
 ;; 1.
+
+; String String Natural Natural Boolean -> restaurant
+; consumes a name, food, tables, seats, and vegetarian? to produce a restaurant
+; A restaurant consists of: A name(the name of the restaurant)
+;                           food(type of food the restaurant sells)
+;                           tables(how many tables the restaurant has available)
+;                           seats(how many seats the restaurant has available)
+;                           vegetarian?(if the restaurant is vegtarian or not)
 (define-struct restaurant (name food tables seats vegetarian?))
 
 (define R1 (make-restaurant "Le Pain" "French" 10 40 true))
 (define R2 (make-restaurant "Anthony" "Pizza" 5 20 false))
 (define R3 (make-restaurant "Longhorn" "Steakhouse" 50 250 true))
 
+;;----------------------------------------------------------------------------------------
 ;; 2.
+
 ;;constructor
 ;;make-restaurant: String String Number Number Boolean -> restaurant (make-restaurant...)
 
@@ -23,6 +35,7 @@
 ;;predicate:
 ;;restaurant?: Anytype -> Boolean
 
+;;----------------------------------------------------------------------------------------
 ;; 3.
 
 ;;restaurant-type: restaurant -> String
@@ -32,22 +45,40 @@
         [(boolean=? (restaurant-vegetarian? restaurant) true) "vegetarian-friendly"]
         [else (restaurant-food restaurant)]))
 
+;; Test Cases:
 (check-expect (restaurant-type R1) "vegetarian-friendly")
 (check-expect (restaurant-type R2) "Pizza")
 (check-expect (restaurant-type R3) "event venue")
 
+;;----------------------------------------------------------------------------------------
 ;; 4.
+
+; Natural Natural Natural -> date
+; consumes a month, day, and year to produce a date
+; A date consists of: A month(the month of th year)
+;                     day(which day of the month)
+;                     year(which year it is)
 (define-struct date (month day year))
+
+
+; String String String Natural Natural -> reservation
+; consumes a restaurant-name, person, phone, date, and party to produce a reservation
+; A restaurant consists of: A restaurant-name(the name of the restaurant)
+;                           person(the name of the person making the reservation)
+;                           phone(their phone number)
+;                           datewhat date the reservation is for)
+;                           party(how many people are in their party)
 (define-struct reservation (restaurant-name person phone date party))
 
 (define Res (make-reservation "Le Pain" "Bob" "111-212-3333" (make-date 9 6 2020) 12))
 (define Res1 (make-reservation "Anthony" "Joe" "123-212-3333" (make-date 9 4 2020) 10))
 (define Res2 (make-reservation "Longhorn" "Lily" "511-212-3233" (make-date 9 9 2020) 4))
 
+;;----------------------------------------------------------------------------------------
 ;; 5.
+
 ;;add-to-party: reservation Number -> reservation
 ;;consumes a reservation and number and produces a reservation party increased by the number
-
 (define (add-to-party reservation people)
    (make-reservation (reservation-restaurant-name reservation)
                     (reservation-person reservation)                    
@@ -55,26 +86,23 @@
                     (reservation-date reservation)
                     (+ (reservation-party reservation) people)))
 
+;; Test Cases:
 (check-expect (add-to-party Res 12) (make-reservation "Le Pain" "Bob" "111-212-3333" (make-date 9 6 2020) 24))
 (check-expect (add-to-party Res1 -6) (make-reservation "Anthony" "Joe" "123-212-3333" (make-date 9 4 2020) 4))
 (check-expect (add-to-party Res2 0) (make-reservation "Longhorn" "Lily" "511-212-3233" (make-date 9 9 2020) 4))
 
+;;----------------------------------------------------------------------------------------
 ;; 6.
 
 ;;precedes: Number Number -> Boolean
 ;;Takes two dates and returns true if first date precedes second
-
-;(define (precedes? date1 date2)
-;  (> date1 date2))
 (define (precedes? date1 date2)
-  (if (> (date-year date1) (date-year date2))
-      false
-     (if (> (date-month date1) (date-month date2))
-         false
-         (if (>= (date-day date1) (date-day date2))
-             false
-             true))))
+  (cond [(> (date-year date1) (date-year date2)) false]
+        [(> (date-month date1) (date-month date2)) false]
+        [(>= (date-day date1) (date-day date2)) false]
+        [else true]))
 
+;; Test Cases:
 (check-expect (precedes? (make-date 9 6 2020) (make-date 9 10 2020)) true)
 (check-expect (precedes? (make-date 9 4 2020) (make-date 9 2 2020)) false)
 (check-expect (precedes? (make-date 9 1 2020) (make-date 9 1 2020)) false)
@@ -82,17 +110,20 @@
 (check-expect (precedes? (make-date 9 2 2001) (make-date 9 5 2004)) true)
 (check-expect (precedes? (make-date 9 2 2010) (make-date 9 5 2008)) false)
 
+;;----------------------------------------------------------------------------------------
 ;; 7.
 
 ;;reservation-OK?: reservation number -> boolean
-;;consumes reservation and date to get OK or Not OK
-
+;;consumes reservation and date to get OK or not OK
 (define (reservation-OK? reservation today-date restaurant)
   (cond [(or (precedes? (reservation-date reservation) today-date)
              (< (restaurant-seats restaurant) (reservation-party reservation))) "not OK"]
         [else "OK"]))
 
+;; Test Cases: 
 (check-expect (reservation-OK? Res (make-date 9 9 2020) R1) "not OK")
 (check-expect (reservation-OK? Res (make-date 9 4 2020) R1) "OK")
+
+;;----------------------------------------------------------------------------------------
 
 
