4347d9e0402d0fa81012c4fbeab5e7024fde8be6
Miles <milesgregg3@gmail.com>
2020-10-09 16:37:59 -0400

    fixed 3 and 4

--- nguyen-a-gregg-m-hw6.rkt
+++ nguyen-a-gregg-m-hw6.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname |Lab 6|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname nguyen-a-gregg-m-hw6) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;; 1
 
 (define-struct course (department course-number faculty max-enrollment students))
@@ -17,7 +17,7 @@
 ;;       - empty
 ;;       - (cons Student ListOfStudent)
 
-(define-struct student (name ID courses))
+(define-struct student (name id courses))
 
 ;; a Student is a (make-student String Number aloc)
 ;; interp: represents a student along with the course they are enrolled in
@@ -32,36 +32,50 @@
 ;; 2
 
 ;; c represents a list of courses that is empty
-(define c (list ))
+(define courses empty)
 
 ;; s represents a list of students that is empty
-(define s (list ))
+(define students empty)
 
 ;; 3
 
-(define (add-student n id)
-  (if (string? (student-id? s n id))
+(define (add-student name id)
+  (if (student-id-in-list? students name id)
       "student already exists"
-      (set! s (student-id? s n id))))
+      (set! students (cons (make-student name id empty) students))))
 
-(define (student-id? alos n id)
-  (cond [(empty? alos) (cons (make-student n id empty) s)]
-        [else
-          (if (not (= id (student-ID (first s))))
-              (student-id? (rest alos) n id)
-              "student already exists")]))
-              
+(define (student-id-in-list? alos name id)
+  (cond [(empty? alos) false]
+        [(cons? alos) (if (= id (student-id (first alos)))
+                          true
+                          (student-id-in-list? (rest alos) name id))]))
 
 ;; 4
 
 (define (add-course dept course-num faculty max-enroll)
-  (if (string? (course?? c dept course-num faculty max-enroll))
+  (if (course-in-list? courses dept course-num)
       "course already exists"
-  (set! c (course?? c dept course-num faculty max-enroll))))
+      (set! courses (cons (make-course dept course-num faculty max-enroll empty)))))
 
-(define (course?? aloc dept course-num faculty max-enroll)
-  (cond [(empty? aloc) (cons (make-course dept course-num faculty max-enroll empty) c)]
-        [else
-         (if (and (string=? dept (course-department (first aloc))) (= course-num (course-course-number (first aloc))))
-             "course already exists"
-             (course?? (rest aloc) dept course-num faculty max-enroll))]))
+(define (course-in-list? aloc dept course-num)
+  (cond [(empty? aloc) false]
+        [(cons? aloc) (if (and (string=? dept (course-department (first aloc)))
+                               (= course-num (course-course-number (first aloc))))
+                          true
+                          (course-in-list? (rest aloc) dept course-num))]))
+
+;(define (add-course dept course-num faculty max-enroll)
+;  (if (string? (course?? c dept course-num faculty max-enroll))
+;      "course already exists"
+;  (set! c (course?? c dept course-num faculty max-enroll))))
+;
+;(define (course?? aloc dept course-num faculty max-enroll)
+;  (cond [(empty? aloc) (cons (make-course dept course-num faculty max-enroll empty) c)]
+;        [else
+;         (if (and (string=? dept (course-department (first aloc))) (= course-num (course-course-number (first aloc))))
+;             "course already exists"
+;             (course?? (rest aloc) dept course-num faculty max-enroll))]))
+
+;; 5
+
+(define)
